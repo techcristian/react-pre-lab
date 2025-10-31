cd089ec3a84985041211783184927842bf442f65
techcristian <100042922+techcristian@users.noreply.github.com>
2025-10-31 02:58:22 -0300

    Modificacion de funcionalidades de la clase 9,funciones  addItem y getTotalItems

--- src/App.jsx
+++ src/App.jsx
@@ -18,7 +18,7 @@ function App() {
         <Routes>
           <Route path={"/"} element={<ItemListContainer />} />
           <Route path={"/detail/:id"} element={<ItemDetailContainer />} />
-          <Route path={"/carrito"} element={<h1>Carrito</h1>}/>
+          
         </Routes>
          <Footer />
       </CartProvider>
