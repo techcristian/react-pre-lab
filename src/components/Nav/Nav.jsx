3084f84050dc165b3b2fbd26139870fd7a428d09
techcristian <100042922+techcristian@users.noreply.github.com>
2025-11-04 21:01:04 -0300

    img en nav

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,4 +1,5 @@
 import { Link } from "react-router-dom"
+import logo from "../../../public/images/casio-sumergible.webp"
 import './Nav.css'
 
 import {useCartContext} from '../../context/CartContex/useCartContext';
@@ -6,9 +7,14 @@ export const Nav = () => {
   const {getTotalItems} = useCartContext();
   return (
     <nav>
-     <Link to={"/"}> <h1 className="logo">Relojeria</h1></Link>
+        <div className="nav-logo">
+        <Link to="/">
+          <img src={logo} alt="Logo de la tienda" className="logo-img" />
+        </Link>
+       <Link to={"/"}> <h1 className="logo-text">CasioStore</h1></Link>
+      </div>
     <ul>
-      <li><Link to={"/"}>  Home</Link> </li>
+     
       <li><Link to={"/category/analogico"}>  Analogico</Link> </li>
       <li><Link to={"/category/deportivo"}>  Deportivo</Link> </li>
       <li><Link to={"/category/digital"}>  Digital</Link> </li>
